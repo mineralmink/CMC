cd7bc52ac5a82346afc3b8fb666c04fcd536a4bb
donus <donus33@gmail.com>
2015-09-14 14:32:28 +0700

    OOADDDEEE

---
+++ app/src/main/java/donuseiei/test/com/authen/MainActivity.java
@@ -0,0 +1,34 @@
+package donuseiei.test.com.authen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (Button)findViewById(R.id.button_login);
+        register = (Button)findViewById(R.id.button_Register);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

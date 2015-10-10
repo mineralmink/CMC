ce99c748ecf5a903d65dee2c2df17758580ca9eb
Pongpayak Handsome <donus33@gmail.com>
2015-10-10 22:11:34 +0700

    add profile button on actionbar

--- app/src/main/java/donuseiei/test/com/authen/MainActivity.java
+++ app/src/main/java/donuseiei/test/com/authen/MainActivity.java
@@ -1,11 +1,20 @@
 package donuseiei.test.com.authen;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements Plan_page.OnFragmentInteractionListener{
+    RadioButton dash;
+    RadioButton plan;
+    RadioButton bill;
+    RadioButton report;
+    RadioButton mail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction_plan = getSupportFragmentManager().beginTransaction();
         transaction_plan.replace(R.id.container_ac, fragment_plan);
         transaction_plan.commit();
+        //onClick();
+    }
+    private void onClick(){
+        dash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Dash_page fragment_dash = new Dash_page();
+                FragmentTransaction transaction_plan = getSupportFragmentManager().beginTransaction();
+                transaction_plan.replace(R.id.container_ac, fragment_dash);
+                transaction_plan.commit();*/
+            }
+        });
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- app/src/main/java/donuseiei/test/com/authen/Plan_page.java
+++ app/src/main/java/donuseiei/test/com/authen/Plan_page.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
@@ -22,7 +23,6 @@ public class Plan_page extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -64,7 +64,8 @@ public class Plan_page extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan_page, container, false);
+        View v = inflater.inflate(R.layout.fragment_plan_page, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event

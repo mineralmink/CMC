2bc8ec6cc80c301018e44b9dd0d06a15dfe03284
Pongpayak Handsome <donus33@gmail.com>
2015-10-11 23:55:18 +0700

    add every page(beta) add tab on dash and plan add button menu change color when clicked

--- app/src/main/java/donuseiei/test/com/authen/MainActivity.java
+++ app/src/main/java/donuseiei/test/com/authen/MainActivity.java
@@ -1,45 +1,116 @@
 package donuseiei.test.com.authen;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity implements Plan_page.OnFragmentInteractionListener{
-    RadioButton dash;
-    RadioButton plan;
-    RadioButton bill;
-    RadioButton report;
-    RadioButton mail;
+import java.util.Objects;
+
+import donuseiei.test.com.authen.page.Bill_page;
+import donuseiei.test.com.authen.page.Dash_page;
+import donuseiei.test.com.authen.page.Mail_page;
+import donuseiei.test.com.authen.page.Plan_page;
+import donuseiei.test.com.authen.page.Profile_page;
+import donuseiei.test.com.authen.page.Report_page;
+
+public class MainActivity extends AppCompatActivity
+        implements Plan_page.OnFragmentInteractionListener,
+        Login_page.OnFragmentInteractionListener,
+        Registe_page.OnFragmentInteractionListener,
+        Dash_page.OnFragmentInteractionListener,
+        Report_page.OnFragmentInteractionListener,
+        Bill_page.OnFragmentInteractionListener,
+        Mail_page.OnFragmentInteractionListener,
+        Profile_page.OnFragmentInteractionListener{
+
+    private RadioButton dash;
+    private RadioButton plan;
+    private RadioButton bill;
+    private RadioButton report;
+    private RadioButton mail;
+    private RadioButton tog;
+    private final int oldColor = Color.rgb(0, 92, 169); //tab color before clicked
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Plan_page fragment_plan = new Plan_page();
-        FragmentTransaction transaction_plan = getSupportFragmentManager().beginTransaction();
-        transaction_plan.replace(R.id.container_ac, fragment_plan);
-        transaction_plan.commit();
-        //onClick();
+        //construct tab button
+        dash = (RadioButton)findViewById(R.id.btnDash);
+        plan = (RadioButton)findViewById(R.id.btnPlan);
+        bill = (RadioButton)findViewById(R.id.btnBill);
+        report = (RadioButton)findViewById(R.id.btnReport);
+        mail = (RadioButton)findViewById(R.id.btnMail);
+        //default
+        tog = dash;
+        toggle(dash);
+        replacePage(new Dash_page());
+        //check button menu clicked
+        onClick();
     }
+    /*
+    * check tab clicked and change page
+    * */
     private void onClick(){
         dash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* Dash_page fragment_dash = new Dash_page();
-                FragmentTransaction transaction_plan = getSupportFragmentManager().beginTransaction();
-                transaction_plan.replace(R.id.container_ac, fragment_dash);
-                transaction_plan.commit();*/
+                toggle(dash);
+                replacePage(new Dash_page());
+            }
+        });
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle(plan);
+                replacePage(new Plan_page());
+            }
+        });
+        bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle(bill);
+                replacePage(new Bill_page());
+            }
+        });
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle(report);
+                replacePage(new Report_page());
+            }
+        });
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle(mail);
+                replacePage(new Mail_page());
             }
         });
     }
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
+    /*
+    * change page
+    * */
+    public void replacePage(Fragment f){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_ac, f);
+        transaction.commit();
+    }
+    /*
+    * set color background when tab clicked
+    * */
+    public void toggle(RadioButton radioButton){
+        tog.setBackgroundColor(oldColor);
+        radioButton.setBackgroundColor(Color.GRAY);
+        tog = radioButton;
     }
 
     @Override
@@ -48,5 +119,23 @@ public class MainActivity extends AppCompatActivity implements Plan_page.OnFragm
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.profile:
+                tog.setBackgroundColor(oldColor);
+                replacePage(new Profile_page());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //
     }
 }
--- app/src/main/java/donuseiei/test/com/authen/page/Mail_page.java
+++ app/src/main/java/donuseiei/test/com/authen/page/Mail_page.java
@@ -1,4 +1,4 @@
-package donuseiei.test.com.authen;
+package donuseiei.test.com.authen.page;
 
 import android.app.Activity;
 import android.net.Uri;
@@ -7,22 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
 
+import donuseiei.test.com.authen.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Plan_page.OnFragmentInteractionListener} interface
+ * {@link Mail_page.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Plan_page#newInstance} factory method to
+ * Use the {@link Mail_page#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Plan_page extends Fragment {
+public class Mail_page extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -35,11 +36,11 @@ public class Plan_page extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Plan_page.
+     * @return A new instance of fragment Mail_page.
      */
     // TODO: Rename and change types and number of parameters
-    public static Plan_page newInstance(String param1, String param2) {
-        Plan_page fragment = new Plan_page();
+    public static Mail_page newInstance(String param1, String param2) {
+        Mail_page fragment = new Mail_page();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -47,7 +48,7 @@ public class Plan_page extends Fragment {
         return fragment;
     }
 
-    public Plan_page() {
+    public Mail_page() {
         // Required empty public constructor
     }
 
@@ -64,8 +65,7 @@ public class Plan_page extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_plan_page, container, false);
-        return v;
+        return inflater.inflate(R.layout.fragment_mail_page, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
